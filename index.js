16857ce15e3034f9f80a807935d767790519479e
Yazan Khirfan <yazankhirfan@gmail.com>
2020-02-10 09:50:56 -0500

    delete homepage.html & add heroku server port

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ app.post("/send", function(req, res) {
 
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 
     console.log("Server is running on port 3000.");
 });
